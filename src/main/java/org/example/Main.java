2d1f1226ba79e4ee4c02bb95c2fd64acdc9b708b
YesSeri <34012616+YesSeri@users.noreply.github.com>
2022-09-26 14:26:25 +0200

    First commit. Adds two numbers.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Skriv et nummer.");
+        int num1 = sc.nextInt();
+        System.out.println("Skriv et nummer mer.");
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Summen er: " + sum);
+    }
+}
